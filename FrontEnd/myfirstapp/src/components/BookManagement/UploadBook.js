399414929a481517591edf5c65c1ff11d200e446
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-08-29 13:36:39 +1000

    Replaced FrontEnd with that of BK-20-upload, npm starts correctly

---
+++ FrontEnd/myfirstapp/src/components/BookManagement/UploadBook.js
@@ -0,0 +1,286 @@
+import React, { Component } from 'react'
+import "./UploadBook.css";
+import PropTypes from "prop-types";
+import CurrencyInput from 'react-currency-input-field';
+import { bookActions } from "../../actions/bookActions";
+
+
+class UploadBook extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            title: "",
+            author: "",
+            genre: "",
+            type: "",
+            price: "",
+            publisher: "",
+            date_of_publication: "",
+            tagline: "",
+            table_of_contents: "",
+            blurb: "",
+            bookCover: ""
+        };  
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        e.target.className += " was-validated";
+        const newBook = {
+            title: this.state.title,
+            author: this.state.author,
+            genre: this.state.genre,
+            type: this.state.type,
+            price: this.state.price,
+            publisher: this.state.publisher,
+            date_of_publication: this.state.date_of_publication,
+            table_of_contents: this.state.table_of_contents,
+            tagline:this.state.tagline,
+            blurb: this.state.blurb,
+            bookCover: this.state.bookCover
+        }
+        // console.log(newBook);
+        // this.props.createBook(newBook, this.props.history);
+    }
+
+    /* Reads the image file for preview */
+    imageHandler = (e) => {
+        const bookCoverReader = new FileReader();
+        bookCoverReader.onload = () => {
+            if (bookCoverReader.readyState === 2) {
+                this.setState({bookCover: bookCoverReader.result})
+            }
+        }
+        bookCoverReader.readAsDataURL(e.target.files[0])
+    }  
+
+    render() {
+        const { errors, bookCover } = this.state;
+        return (
+            <div className="uploadBook">
+                <div className="container">
+                    <h1 className="display-4 text-center">Upload Book</h1>
+                    <p className="lead text-center">Add a new or second hand book below</p>
+                    <div className="row">
+                        <div className="col">
+                            <form className="needs-validation" onSubmit={this.onSubmit} noValidate>
+                            {/*<form onSubmit={this.onSubmit}>*/}
+
+                                {/*Title*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Title</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Title"
+                                        name="title"
+                                        value={this.state.title}
+                                        onChange={this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Title cannot be Empty.
+                                    </div>
+                                </div>
+
+                                {/*Author*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Author</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Author"
+                                        name="author"
+                                        value= {this.state.author}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Author cannot be Empty.
+                                    </div>
+                                </div>
+
+                                {/*Genre*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Genre</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Genre"
+                                        name="genre"
+                                        value= {this.state.genre}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Genre cannot be Empty.
+                                    </div>
+                                </div>
+
+                                {/*Type of Book*/}
+                                <div className="input-group mb-2">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="basic-addon1">Type</span>
+                                    </div>
+                                    <select className=" custom-select form-control form-control-lg" required>
+                                        <option selected disabled value="">Select type</option>
+                                        <option value="personal">Personal Book</option>
+                                        <option value="new">New Book</option>
+                                        <option value="old">Second Hand Book</option>
+                                    </select>
+                                    <div className="invalid-feedback">
+                                        Need to select an option.
+                                    </div>
+                                </div>
+
+                                {/*Price*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Price</label>
+                                    <CurrencyInput
+                                        intlConfig={{ locale: 'en-US', currency: 'AUD' }}
+                                        className="form-control form-control-lg"
+                                        name="price"
+                                        placeholder="Price"
+                                        decimalsLimit={2}
+                                        onValueChange={(value, name) => console.log(value, name)}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Book needs a price.
+                                    </div>
+                                </div>
+
+                                {/*Publisher*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Publisher</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Publisher"
+                                        name="publisher"
+                                        value= {this.state.publisher}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Publisher cannot be Empty.
+                                    </div>
+                                </div>
+
+                                {/*Date of Publication*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Date of Publication</label>
+                                    <input
+                                        type="date"
+                                        className="form-control form-control-lg"
+                                        placeholder="Date of Publication"
+                                        name="date_of_publication"
+                                        value= {this.state.date_of_publication}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Date cannot be Empty.
+                                    </div>
+                                </div>
+
+                                {/*Tagline*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Tagline</label>
+                                    <textarea
+                                        type="paragraph"
+                                        className="form-control form-control-lg"
+                                        placeholder="Add Taglines Here"
+                                        name="tagline"
+                                        rows={7}
+                                        cols={7}
+                                        value= {this.state.tagline}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Please enter at least one tagline.
+                                    </div>
+                                </div>
+
+                                {/*Table of Contents*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Table of Contents</label>
+                                    <textarea
+                                        type="paragraph"
+                                        className="form-control form-control-lg"
+                                        placeholder="Add Table Of Contents here"
+                                        name="table_of_contents"
+                                        rows={10}
+                                        cols={10}
+                                        value= {this.state.table_of_contents}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Every book needs a Table of Contents.
+                                    </div>
+                                </div>
+
+                                {/*Blurb*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Blurb</label>
+                                    <textarea
+                                        type="paragraph"
+                                        className="form-control form-control-lg"
+                                        placeholder="Add Blurb here"
+                                        name="blurb"
+                                        rows={10}
+                                        cols={10}
+                                        value= {this.state.blurb}
+                                        onChange = {this.onChange}
+                                        required
+                                    />
+                                    <div className="invalid-feedback">
+                                        Every book needs a Blurb.
+                                    </div>
+                                </div>
+
+                                {/*Book Cover*/}
+                                <div className="form-group">
+                                    <label htmlFor="priceInput">Upload Book Cover</label>
+                                    <input
+                                        type="file"
+                                        className="form-control-file"
+                                        name="bookCover"
+                                        accept="image/*"
+                                        onChange={this.imageHandler}
+                                        required
+                                    />
+                                    {/*Book Cover Preview*/}
+                                    <div class="col-xs-6 col-md-3">
+                                        <img src={this.state.bookCover} />
+                                    </div>
+                                    <div className="invalid-feedback">
+                                        Need a Book Cover.
+                                    </div>
+                                </div>
+                                <input type="submit" className="btn btn-primary btn-lg btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+UploadBook.propTypes = {
+    // createProject: PropTypes.func.isRequired
+};
+
+export default UploadBook;
---
+++ FrontEnd/myfirstapp/src/components/Layout/Landing.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        {/*Frontpage showcase*/}
+        {/*<div className="text-panel text-white">*/}
+        {/*  <div className="caption">*/}
+        {/*    <h1 className="display-3 mb-4">Bookeroo</h1>*/}
+        {/*    <h3 className="display-6 mb-1">Passionate About Books</h3>*/}
+        {/*    <br />*/}
+        {/*    <p><a className="btn btn-success btn-lg" href="/register" role="button">Sign Up Now</a></p>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+        <div className="landing-inner light-overlay text-dark">
+          <div className="container">
+            <div className="row">
+
+              {/*Greeter*/}
+              <div className="col">
+                <div className="text-panel text-white">
+                  <div className="caption">
+                    <h1 className="display-3 mb-1" style={{fontSize:"3.3em"}}>Bookeroo</h1>
+                    <h3 className="display-6 mb-3">Passionate About Books</h3>
+                    <br />
+                    <p><a className="btn btn-lg btn-success " href="/register" role="button">Sign Up Now</a></p>
+                  </div>
+                </div>
+              </div>
+
+              {/*Development Landing*/}
+              <div className="col">
+                <div className="development-landing">
+                  <h1 className="display-3 mb-4" style={{fontSize:"3.3em"}}>Development Landing</h1>
+                  <p className="lead">
+                    Below are the available front-end pages that have been developed.
+                  </p>
+                  <hr />
+                  <Link className="btn btn-lg btn-secondary mb-2 mr-2" to="/register">
+                    Sign Up
+                  </Link>
+                  <Link className="btn btn-lg btn-secondary mb-2 mr-2" to="/login">
+                    Login
+                  </Link>
+                  <Link className="btn btn-lg btn-primary mb-2 mr-2" to="/publisher">
+                    Publisher Landing
+                  </Link>
+                  <Link className="btn btn-lg btn-secondary mb-2 mr-2" to="/addPerson">
+                    Add Person
+                  </Link>
+                  <Link className="btn btn-lg btn-secondary mb-2 mr-2" to="/dashboard">
+                    Dashboard
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
