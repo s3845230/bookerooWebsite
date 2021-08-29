ef8abaea40989a7577076d9855408f6337bec3ab
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-08-29 14:12:25 +1000

    Updated Book.java with fields from BK-20-upload-book

--- BackEnd/bookmicroservice/src/main/java/com/rmit/sept/bookmicroservice/model/Book.java
+++ BackEnd/bookmicroservice/src/main/java/com/rmit/sept/bookmicroservice/model/Book.java
@@ -1,6 +1,5 @@
 package com.rmit.sept.bookmicroservice.model;
 
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -9,47 +8,94 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long bookId;
 
+    //this probably needs @min @max if isbn has a set size
+//    @NotBlank(message = "Book requires isbn")
+    private String isbn;
+
+    //    @NotBlank(message = "Book requires title")
     private String title;
+
+    //    @NotBlank(message = "Book requires author")
+//    @Size(min = 3, max = 20, message = "3 to 20 chars")
     private String author;
-    
 
-    private Date created_date;
-    private Date updated_date;
-    
+    //    @NotBlank(message = "Book requires genre")
+    private String genre;
+
+    //    @NotBlank(message = "Book requires type")
+    private String type;
+
+    //    @NotBlank(message = "Book requires price")
+    private Double price;
+
+    //    @NotBlank(message = "Book requires genre")
+    private String publisher;
+
+    //    @NotBlank(message = "Book requires publicationDate")
+    private Date publicationDate;
+
+    //    @NotBlank(message = "Book requires tagline")
+    private String tagLine;
+
+    //    @NotBlank(message = "Book requires tableOfContents")
+    private String tableOfContents;
+
+    //    @NotBlank(message = "Book requires blurb")
+    private String blurb;
+
+    //    @NotBlank(message = "Book requires imageHash")
+    @Lob
+    private byte[] imageBlob;
+
+    private Date created_at;
+    private Date updated_at;
+
+    public Long getId() {return bookId;}
+    public void setId(Long bookId) {this.bookId = bookId;}
+
+    public String getIsbn() {return isbn;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+
+    public String getAuthor() {return author;}
+    public void setAuthor(String author) {this.author = author;}
+
+    public String getGenre() {return genre;}
+    public void setGenre(String genre) {this.genre = genre;}
+
+    public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
+
+    public Double getPrice() {return price;}
+    public void setPrice(Double price) {this.price = price;}
+
+    public String getPublisher() {return publisher;}
+    public void setPublisher(String publisher) {this.publisher = publisher;}
+
+    public Date getPublicationDate() {return publicationDate;}
+    public void setPublicationDate(Date publicationDate) {this.publicationDate = publicationDate;}
+
+    public String getTagLine() {return tagLine;}
+    public void setTagLine(String tagLine) {this.tagLine = tagLine;}
+
+    public String getTableOfContents() {return tableOfContents;}
+    public void setTableOfContents(String tableOfContents) {this.tableOfContents = tableOfContents;}
+
+    public String getBlurb() {return blurb;}
+    public void setBlurb(String blurb) {this.blurb = blurb;}
+
+    public byte[] getImageBlob() {return imageBlob;}
+    public void setImageBlob(byte[] imageBlob) {this.imageBlob = imageBlob;}
+
     @PrePersist
-    protected void onCreate() {
-        this.created_date = new Date();
+    protected void onCreation(){
+        this.created_at = new Date();
     }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updated_date = new Date();
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String name) {
-        this.title = name;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String number) {
-        this.author = number;
-    }
-    public void setCreated_date(Date created_date) {
-        this.created_date = created_date;
-    }
-    public void setUpdated_date(Date updated_date) {
-        this.updated_date = updated_date;
+    protected void onUpdate(){
+        this.updated_at = new Date();
     }
 }
