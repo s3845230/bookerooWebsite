3e7fa631251747fd4a33609ad1c3e54b473e129d
Charlton <s3845107@student.rmit.edu.au>
2021-09-18 14:44:59 +1000

    added book cover to search results page

--- FrontEnd/myfirstapp/src/components/ViewBooks/SearchResults.js
+++ FrontEnd/myfirstapp/src/components/ViewBooks/SearchResults.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./SearchResults.css";
 
 class SearchResults extends Component {
     render() {
@@ -6,7 +7,12 @@ class SearchResults extends Component {
             <div className="searchResults">
                 <div className="container">
                     <h1 className="display-4 text-center">Showing Results For</h1>
-                        {this.props.location.state.books.map((book) => <h3 key={book.id}>{book.title}</h3>)}
+                        {this.props.location.state.books.map((book) => 
+                            <div className="row" key={book.id}>
+                                <img key={book.id} className="cover rounded float-left img-thumbnail" src={`${book.imageType},${book.imageBlob}`} />
+                                <h3>{book.title}</h3>
+                            </div>
+                        )}
                 </div>
             </div>
         )
