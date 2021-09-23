38303e82a2cd97d53c4cadfee7808959dcae9603
Charlton <s3845107@student.rmit.edu.au>
2021-09-23 19:13:36 +1000

    adjusted layout for admin functions page

--- FrontEnd/bookeroo/src/components/Admin/AdminFunctions.js
+++ FrontEnd/bookeroo/src/components/Admin/AdminFunctions.js
@@ -8,20 +8,26 @@ class AdminFunctions extends Component {
                 <div className="light-overlay landing-inner text-dark">
                     <div className="container">
                         <div className="row">
-                            <div className="col-md-12 text-left">
-                                <h1 className="display-4 text-left">Admin Dashboard</h1>
-                                <Link className="btn btn-lg btn-primary mb-2 mr-2" to="/admin/addUser">
-                                    Add User
-                                </Link>
-                                <Link className="btn btn-lg btn-primary mb-2 mr-2" to="/admin/addBook">
-                                    Add Book
-                                </Link>
-                                <Link className="btn btn-lg btn-primary mb-2 mr-2" to="/admin/viewAllUsers">
-                                    View All Users
-                                </Link>
-                                <Link className="btn btn-lg btn-primary mb-2 mr-2" to="/admin/viewAllBooks">
-                                    View All Books
-                                </Link>
+                            <div className="col-md-12 text-center">
+                                <h1 className="display-4">Admin Dashboard</h1>
+                                <div className="row">
+                                    <div className="col">
+                                        <h1 className="lead">User Management</h1>
+                                        <Link className="btn btn-lg btn-primary mb-5 mr-3" to="/admin/addUser">
+                                            Add User
+                                        </Link>
+                                        <Link className="btn btn-lg btn-primary mb-5 mr-3" to="/admin/viewAllUsers">
+                                            View All Users
+                                        </Link>
+                                        <h1 className="lead">Book Management:</h1>
+                                        <Link className="btn btn-lg btn-primary mb-5 mr-3" to="/admin/addBook">
+                                            Add Book
+                                        </Link>
+                                        <Link className="btn btn-lg btn-primary mb-5 mr-3" to="/admin/viewAllBooks">
+                                            View All Books
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
