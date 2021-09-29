1f3187ae75d3ab5451bce6f77dc86cfcb054e51f
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-09-29 17:10:01 +1000

    Implementented decoded JWT token to local storage - UserService methods available to allow ease of access

--- FrontEnd/bookeroo/src/actions/types.js
+++ FrontEnd/bookeroo/src/actions/types.js
@@ -2,3 +2,4 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_ALL_BOOKS = "GET_ALL_BOOKS";
 export const GET_BOOKS = "GET_BOOKS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_CURRENT_ROLE = "GET_CURRENT_ROLE";
