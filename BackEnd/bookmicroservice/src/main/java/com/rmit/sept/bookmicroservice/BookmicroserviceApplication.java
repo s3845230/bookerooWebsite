191bd9e4cceffd08168c4aa83c88542e2a3e32e1
s3845230 <s3845230@student.rmit.edu.au>
2021-10-20 18:09:27 +1100

    added xml and dependencies for logging

--- BackEnd/bookmicroservice/src/main/java/com/rmit/sept/bookmicroservice/BookmicroserviceApplication.java
+++ BackEnd/bookmicroservice/src/main/java/com/rmit/sept/bookmicroservice/BookmicroserviceApplication.java
@@ -3,11 +3,20 @@ package com.rmit.sept.bookmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class BookmicroserviceApplication {
+public class BookmicroserviceApplication extends SpringBootServletInitializer {
+
+    private static final Logger LOGGER = LogManager.getLogger(BookmicroserviceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BookmicroserviceApplication.class, args);
+        LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
     }
 
 }
