4103e1b80aca1fb9312ba3f23bd8e71c4a8fa6be
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-10-23 11:02:46 +1100

    Moved authmicroservice to internet-facing load balancer in AWS, updated address in FrontEnd

--- FrontEnd/bookeroo/src/constants.js
+++ FrontEnd/bookeroo/src/constants.js
@@ -1,4 +1,4 @@
 // AWS Deployment Address:
-export const AUTHMICROSERVICE_IP = "http://internal-authmicroservice-balancer-176433370.ap-southeast-2.elb.amazonaws.com";
+export const AUTHMICROSERVICE_IP = "http://authmicroservice-lb-1927247317.ap-southeast-2.elb.amazonaws.com";
 // Local Development Address:
 // export const AUTHMICROSERVICE_IP = "http://localhost:8080";
