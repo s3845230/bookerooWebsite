b98a40a3cd59517ba06dc87a5be2d495d26de5d8
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-10-23 12:12:47 +1100

    Re-enabled CORS, fixed potential typo in SecurityConstant

--- BackEnd/authmicroservice/src/main/java/com/rmit/sept/authmicroservice/security/SecurityConfig.java
+++ BackEnd/authmicroservice/src/main/java/com/rmit/sept/authmicroservice/security/SecurityConfig.java
@@ -49,8 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable()
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
+//        http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
