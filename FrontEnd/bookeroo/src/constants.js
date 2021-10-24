1c47f93f6c61c641913f34498c5f484bd088fd51
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-10-24 13:54:11 +1100

    Updated authmicroservice URL in frontend

--- FrontEnd/bookeroo/src/constants.js
+++ FrontEnd/bookeroo/src/constants.js
@@ -1,4 +1,4 @@
 // AWS Deployment Address:
-// export const AUTHMICROSERVICE_IP = "http://authmicroservice-lb-1717063688.ap-southeast-2.elb.amazonaws.com";
+export const AUTHMICROSERVICE_IP = "http://authmicroservice-lb-1717063688.ap-southeast-2.elb.amazonaws.com";
 // Local Development Address:
-export const AUTHMICROSERVICE_IP = "http://localhost:8080";
+// export const AUTHMICROSERVICE_IP = "http://localhost:8080";
