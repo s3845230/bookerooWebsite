ca9476608861158ad60c6d1d91b08468b43f9158
[Roman O'Brien s3845295] <s3845295@student.rmit.edu.au>
2021-10-24 13:40:03 +1100

    Removed port specification from FRONT_END_URL, should fix CORS pre-flight check

--- BackEnd/authmicroservice/src/main/java/com/rmit/sept/authmicroservice/security/SecurityConstant.java
+++ BackEnd/authmicroservice/src/main/java/com/rmit/sept/authmicroservice/security/SecurityConstant.java
@@ -5,7 +5,7 @@ public class SecurityConstant {
     public static final String AUTH_URL = "/api/auth/**";
     public static final String H2_URL = "/h2-console/**";
 //    public static final String FRONT_END_URL = "http://localhost:3000";
-    public static final String FRONT_END_URL = "http://bookeroo-balancer-2011454518.ap-southeast-2.elb.amazonaws.com:80";
+    public static final String FRONT_END_URL = "http://bookeroo-balancer-2011454518.ap-southeast-2.elb.amazonaws.com";
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
